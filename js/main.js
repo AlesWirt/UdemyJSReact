e0e38c70b54f660da34014996469ae1e2f2f672d
Ales Wirt <sanichwi@gmail.com>
2022-03-04 13:59:58 +0300

    L29. Actions on page

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const box = document.getElementById('box');
 
 console.log(box);
 
-const buttons = document.getElementsByTagName('button')[1];
+const buttons = document.getElementsByTagName('button');
 
 console.log(buttons);
 
@@ -20,3 +20,49 @@ hearts.forEach(item => {
 
 const oneHeart = document.querySelector('.heart');
 console.log(oneHeart);
+
+const wrapper = document.querySelector('.wrapper');
+
+buttons[1].style.borderRadius = '100%';
+circles[0].style.backgroundColor = 'red';
+
+box.style.cssText = 'background-color: blue; width: 500px';
+
+//for(let i = 0; i < hearts.length; i++){
+//	hearts[i].style.backgroundColor = 'crimson';
+//}
+
+hearts.forEach(item => {
+	item.style.backgroundColor = 'rgb(231, 250, 65)';
+});
+
+const div = document.createElement('div');
+//const text = document.createText('There was me'); - old version
+
+div.classList.add('black');
+
+//document.body.append(div);
+
+wrapper.append(div);
+//wrapper.appendChild(div);
+
+//wrapper.insertBefore(div, hearts[0]);
+
+//wrapper.prepend(div);
+
+//hearts[0].before(div);
+
+//hearts[0].after(div);
+
+//circles[0].remove();
+//wrapper.removeChild(hearts[1]);
+
+//hearts[0].replaceWith(circles[0]);
+
+//wrapper.replaceChild(circles[0], hearts[0]);	
+
+div.innerHTML = '<h1>Hello, World</h1>';
+
+//div.textContent = 'Hello';
+
+div.insertAdjacentHTML('beforebegin', '<h2>Hello</h2>');
